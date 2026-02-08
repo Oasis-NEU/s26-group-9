d7a98627b4f16e3dd18eed73ca9991da6bd3d3d6
Gulnas Moshkovich <moshkovich.g@northeastern.edu>
2026-02-08 13:27:27 -0500

    Made Launch Functino

--- react-appp/src/pages/launch.jsx
+++ react-appp/src/pages/launch.jsx
@@ -1,1 +1,11 @@
 // Launch page 
+function Launch() {
+    return (
+        <div>
+            <h1>Launch Page</h1>
+            <p>Welcome to the launch page! Here you can find all the latest updates and information about our product launch.</p>
+        </div>
+    );
+}
+
+export default Launch;
