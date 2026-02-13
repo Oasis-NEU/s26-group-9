654dc68be12c5c9b38d26705fe051406057674df
Gulnas Moshkovich <moshkovich.g@northeastern.edu>
2026-02-12 20:57:51 -0500

    Added Launch css file

--- react-appp/src/pages/launch.jsx
+++ react-appp/src/pages/launch.jsx
@@ -1,20 +1,20 @@
 // Launch page 
 import Button from '@mui/material/Button';
-
+import './Launch.css';
 function Launch() {
-  return (
-    <div>
-      <h1>Launch Page</h1>
-      <p>Welcome to the launch page!</p>
-      <Button variant="contained" color="success" size = "medium">
-      Log-in 
-      </Button>
-      <Button variant="outlined" color="error" size = "medium">
-      Sign-up
-      </Button>
-      <Button color="secondary" size = "medium">Let's go!</Button>
-    </div>
-  );
+    return (
+        <div>
+            <h1>Launch Page</h1>
+            <p>Welcome to the launch page!</p>
+            <Button variant="contained" color="success" size="medium">
+                Log-in
+            </Button>
+            <Button variant="outlined" color="error" size="medium">
+                Sign-up
+            </Button>
+            <Button color="secondary" size="medium">Let's go!</Button>
+        </div>
+    );
 }
 
 export default Launch;
