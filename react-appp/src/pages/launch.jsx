b18235e77f2ecc8217778a1253646b94994da05a
ISABELLA_HE <HE.I@northeastern.edu>
2026-02-12 21:01:04 -0500

    fixed launch page bug

--- react-appp/src/pages/launch.jsx
+++ react-appp/src/pages/launch.jsx
@@ -2,7 +2,6 @@
 import Button from '@mui/material/Button';
 import './Launch.css';
 function Launch() {
-<<<<<<< HEAD
     return (
         <div>
             <h1>Launch Page</h1>
@@ -16,21 +15,5 @@ function Launch() {
             <Button color="secondary" size="medium">Let's go!</Button>
         </div>
     );
-=======
-  return (
-    <div>
-      <h1>ProductiviTea</h1>
-      <p>Welcome to the launch page!</p>
-      <Button variant="contained" color="success" size = "medium">
-      Log-in 
-      </Button>
-      <Button variant="outlined" color="error" size = "medium">
-      Sign-up
-      </Button>
-      <Button color="secondary" size = "medium">Let's go!</Button>
-    </div>
-  );
->>>>>>> 1606f102008003ff237778d35745c7f8cc83ed1e
-}
-
+  }
 export default Launch;
