b0247f305a6d30332e05b730cbea8334210acd85
Gulnas Moshkovich <moshkovich.g@northeastern.edu>
2026-02-21 01:03:28 -0500

    Added Logo

--- react-appp/src/pages/launch.jsx
+++ react-appp/src/pages/launch.jsx
@@ -1,17 +1,21 @@
 // Launch page 
 import Button from '@mui/material/Button';
+import logo from '../public/ProductivitTea Logo.png';
 import './Launch.css';
 function Launch() {
     return (
         <div className="launch-container">
+            <div>
+                <img src={logo} alt="ProductiviTea Logo" className="logo" />
+            </div>
             <h1>ProductiviTea!</h1>
             <p>Welcome to the launch page!</p>
-            <Button className = "myButtonLogIn"
+            <Button className="myButtonLogIn"
                 variant="contained"
                 sx={{
                     padding: '10px 20px',
-                    margin: '20px',        
-                    marginBottom: '200px',    
+                    margin: '20px',
+                    marginBottom: '200px',
                     marginLeft: '200px',
                     bgcolor: '#99836F', // Custom background color
                     '&:hover': {
@@ -24,37 +28,37 @@ function Launch() {
                 size="medium">
                 Log-in
             </Button>
-            <Button className = "myButtonSignUp" 
-            variant="contained" sx={{
-            padding: '10px 20px',
-            bgcolor: '#99836F',
-                '&:hover': {
-                    bgcolor: '#746455',
-                }, 
-                '&:focus': {
+            <Button className="myButtonSignUp"
+                variant="contained" sx={{
+                    padding: '10px 20px',
+                    bgcolor: '#99836F',
+                    '&:hover': {
+                        bgcolor: '#746455',
+                    },
+                    '&:focus': {
                         outline: 'none',
                     },
                     '&.Mui-focusVisible': {
                         outline: '2px #000000',
                     },
-            }} >
+                }} >
                 Sign-up
             </Button>
-            <Button 
-            className = "myButtonLetsGo"
-            variant="contained" sx={{
-                padding: '10px 20px',
-                bgcolor: '#99836F',
-                '&:hover': {
-                    bgcolor: '#746455',
-                },
-                '&:focus': {
+            <Button
+                className="myButtonLetsGo"
+                variant="contained" sx={{
+                    padding: '10px 20px',
+                    bgcolor: '#99836F',
+                    '&:hover': {
+                        bgcolor: '#746455',
+                    },
+                    '&:focus': {
                         outline: 'none',
                     },
                     '&.Mui-focusVisible': {
                         outline: '2px #000000',
                     },
-            }} size="medium">Let's go!</Button>
+                }} size="medium">Let's go!</Button>
         </div>
     );
 }
